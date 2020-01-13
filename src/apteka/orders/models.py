ea633cc6f7cd72f0045cb34443c9a3ac4efe8e24
MikPio <59450936+MikPio@users.noreply.github.com>
2020-01-13 17:31:59 +0100

    Add files via upload

--- src/apteka/orders/models.py
+++ src/apteka/orders/models.py
@@ -1,12 +1,18 @@
+
 from django.db import models
+from accounts.models import User
+from datetime import datetime
+
+from products.models import Product
 
 class Order(models.Model):
-    date    = models.DateTimeField(auto_now=True, auto_now_add=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=datetime.now)
     recived = models.CharField(max_length=150, default="")
     payment = models.CharField(max_length=150, default="")
-    status  = models.CharField(max_length=45, default="")
-
-class OrdersProducts(models.Model):
-    order_id   = models.ForeignKey('Order', on_delete=models.CASCADE, default="")
-    product_id = models.ForeignKey('products.Product', on_delete=models.CASCADE, default="")
-    amount     = models.IntegerField()
+    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
+    price = models.FloatField(blank=True)
+    order_id = models.ForeignKey('Order', on_delete=models.CASCADE)
--- src/apteka/orders/views.py
+++ src/apteka/orders/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Order, OrderItem
+
+##-------------- Order Views --------------------------------------
+class DetailOrder(DetailView):
+    model = Order
+    template_name='orders/detail_order.html'
+
+class ListOrder(ListView):
+    model = Order
+    context_object_name = 'orders'
+    template_name='orders/list_orders.html'
+
+class CreateOrder(CreateView):
+    model = Order
+    template_name = 'orders/create_order.html'
+
+class UpdateOrder(UpdateView):
+    model = Order
+    template_name = 'orders/update_order.html'
+
+class DeleteOrder(DeleteView):
+    model = Order
+    template_name = 'orders/delete_order.html'
+
+
+##-------------- OrderItem Views --------------------------------------
+class DetailOrderItem(DetailView):
+    model = OrderItem
+    template_name='orders/detail_orderitem.html'
+
+class ListOrderItem(ListView):
+    model = OrderItem
+    context_object_name = 'orderitems'
+    template_name='orders/list_orderitems.html'
+
+class CreateOrderItem(CreateView):
+    model = OrderItem
+    template_name = 'orders/create_orderitem.html'
+
+class UpdateOrderItem(UpdateView):
+    model = OrderItem
+    template_name = 'orders/update_orderitem.html'
+
+class DeleteOrderItem(DeleteView):
+    model = OrderItem
+    template_name = 'orders/delete_orderitem.html'
